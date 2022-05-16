f3398d370e682aabac2864e9896622956e53c0a9
Tien <tientv.student@fit.tdc.edu.vn>
2022-05-16 18:28:42 +0700

    init install app

---
+++ BarberShop/BarberShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarberShop
+//
+//  Created by Truong Tien on 5/16/22.
+//  Copyright © 2022 Group 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
