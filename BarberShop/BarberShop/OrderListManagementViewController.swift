da53bf9cfa4f150a26ad94ad9cdeac1355672778
Tien <tienchuong1610@gmail.com>
2022-05-30 16:18:30 +0700

    update

---
+++ BarberShop/BarberShop/OrderListManagementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderListManagementViewController.swift
+//  BarberShop
+//
+//  Created by Truong Tien on 5/29/22.
+//
+
+import UIKit
+
+class OrderListManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BarberShop/BarberShop/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  BarberShop
+//
+//  Created by Truong Tien on 5/29/22.
+//
+
+import Foundation
